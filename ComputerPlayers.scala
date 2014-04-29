66e9a3415ca67ee9d01fe8838a4ada33afc8be30
CapnSmithers <smith.m11@gmail.com>
2014-04-29 12:17:16 -0600

    Added some stubs for comptuer players and game runner stuffs

---
+++ ComputerPlayers.scala
@@ -0,0 +1,17 @@
+/*
+* Code for computer players for Tic Tac Toe
+* Includes both smart and dumb computers, if we 
+* acutally want to implement both.
+* I'm just leaving the dumb computer empty for now
+*/
+
+class SmartComputerPlayer() {
+    // Define necessary methods, etc. 
+    // No constructor needed
+
+}
+
+
+class DumbComputerPlayer() {
+    
+}
---
+++ TicTacToe.scala
@@ -0,0 +1,17 @@
+/* 
+* Main program file for tic tac toe
+* The game is run from the TicTacToe object
+* The game class contains all necessary info to display
+* a board, make a move, and call player or computer moves
+*/
+
+class Game () {
+
+}
+
+object TicTacToe {
+    //Run game from here
+    def main(args: Array[String]) {
+        //Shouldn't need command line args
+    }
+}
